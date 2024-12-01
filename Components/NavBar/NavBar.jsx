85804689d1e9e1815df2400ee96cf937aad428ed
Vibhanshu <vibhanshudeepakdubey@gmail.com>
2024-12-01 21:21:51 +0530

    update navbar

--- Components/NavBar/NavBar.jsx
+++ Components/NavBar/NavBar.jsx
@@ -1,5 +1,12 @@
-import React from 'react'
+import React, {useEffect, useState, useContext} from 'react'
+import Image from "next/image";
+import Link from "next/link";
+
+import { Model, Error } from '../index';
 import Style from './NavBar.module.css'
+import { ChatAppContext } from '@/Context/ChatAppContext';
+
+
 const NavBar = () => {
   return (
     <div>
