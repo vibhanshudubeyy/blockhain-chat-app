a0dc22111075938bcba9dbd17cf20da0baa87f84
Vibhanshu <vibhanshudeepakdubey@gmail.com>
2024-12-02 19:07:42 +0530

    updated navbar

--- Components/NavBar/NavBar.jsx
+++ Components/NavBar/NavBar.jsx
@@ -1,12 +1,11 @@
-import React, {useEffect, useState, useContext} from 'react'
+import React, { useEffect, useState, useContext } from 'react';
 import Image from "next/image";
 import Link from "next/link";
 
 import images from "../../assets";
 import { Model, Error } from '../index';
-import Style from './NavBar.module.css'
+import Style from './NavBar.module.css';
 import { ChatAppContext } from '@/Context/ChatAppContext';
-
 
 const NavBar = () => {
   const menuItems = [
@@ -41,32 +40,91 @@ const NavBar = () => {
   const [openModel, setOpenModel] = useState(false);
 
   const { account, userName, ConnectWallet } = useContext(ChatAppContext);
+
   return (
-    <div className ={Style.NavBar}>
+    <div className={Style.NavBar}>
       <div className={Style.NavBar_box}>
         <div className={Style.NavBar_box_left}>
-          <Image src={images.logo} alt='logo' width={50} height={50}></Image>
+          <Image src={images.logo} alt='logo' width={50} height={50} />
         </div>
         <div className={Style.NavBar_box_right}>
+
+          {/* Desktop */}
           <div className={Style.NavBar_box_right_menu}>
             {menuItems.map((ele, i) => (
-				<div
-					onClick={ () => setActive(i + 1)}
-					key={i + 1}
-					className={`${Style.NavBar_box_right_menu_items} ${active == i + 1 ? Style.active_btn : ""}`}
-				>
+              <div
+                onClick={() => setActive(i + 1)}
+                key={i + 1}
+                className={`${Style.NavBar_box_right_menu_items} ${active == i + 1 ? Style.active_btn : ""}`}
+              >
+                <Link 
+                  className={Style.NavBar_box_right_menu_items_link}
+                  href={ele.link}
+                >
+                  {ele.menu}
+                </Link>
+              </div>
+            ))}
+          </div>
 
-					<Link className={Style.NavBar_box_right_menu_items_link}
-						href={ele.link}>
-							{ele.menu}
-					</Link>
-				</div>
-			))}
+          {/* Mobile */}
+          {open && (
+            <div className={Style.mobile_menu}>
+              {menuItems.map((ele, i) => (
+                <div
+                  onClick={() => setActive(i + 1)}
+                  key={i + 1}
+                  className={`${Style.mobile_menu_items} ${active == i + 1 ? Style.active_btn : ""}`}
+                >
+                  <Link 
+                    className={Style.mobile_menu_items_link}
+                    href={ele.link}
+                  >
+                    {ele.menu}
+                  </Link>
+                </div>
+              ))}
+              <p className={Style.mobile_menu_btn}>
+                <Image 
+                  src={images.close} 
+                  alt='close' 
+                  width={50} 
+                  height={50}
+                  onClick={() => setOpen(false)}
+                />
+              </p>
+            </div>
+          )}
+
+          {/* Connect Wallet */}
+          <div className={Style.NavBar_box_right_connect}>
+            {account == "" ? (
+              <button onClick={() => ConnectWallet()}>
+                <span>Connect Wallet</span>
+              </button>
+            ) : (
+              <button onClick={() => setOpenModel(true)}>
+                <Image 
+                  src={userName ? images.accountName : images.create2}
+                  alt='Account Image'
+                  width={20}
+                  height={20}
+                />
+                <small>{userName || "Create Account"}</small>
+              </button>
+            )}
+          </div>
+
+          <div 
+            className={Style.NavBar_box_right_open}
+            onClick={() => setOpen(true)}
+          >
+            <Image src={images.open} alt='open' width={30} height={30} />
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
